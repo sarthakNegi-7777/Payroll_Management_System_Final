5eda5df468289294d646a5be0f8e79a2f46d0bbe
PrincePandeycode <pandeyprince4723@gmail.com>
2023-11-24 01:13:33 +0530

    created four views of employees view, add , remove , filter

--- Emplyee_app/urls.py
+++ Emplyee_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = "index"),
+    path('',views.index, name = 'index'),
+    path('all_emp',views.index, name = 'all_emp'),
+    path('add_emp',views.index, name = 'add_emp'),
+    path('remove_emp',views.index, name = 'remove_emp'),
+    path('filter_emp',views.index, name = 'filter_emp'),
 ]
