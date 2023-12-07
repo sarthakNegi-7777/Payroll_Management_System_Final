b96015fa9eab9f0f4a9f7cf363c3febde8954c8e
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-07 23:54:50 +0530

    some changes

--- Emplyee_app/urls.py
+++ Emplyee_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('remove_emp',views.remove_emp, name = 'remove_emp'),
     path('remove_emp/<int:emp_id>',views.remove_emp, name = 'remove_emp'),
     path('filter_emp',views.filter_emp, name = 'filter_emp'),
+  
 ]
