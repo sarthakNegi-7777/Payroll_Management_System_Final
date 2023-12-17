b535f7267463134870336f8e978361eacd2cfe83
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-17 23:42:55 +0530

    settings.py removed useless thigs

--- Employee_management/settings.py
+++ Employee_management/settings.py
@@ -96,16 +96,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Emplyee_app/static')]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
