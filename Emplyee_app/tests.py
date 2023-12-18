3cfa81dd722a32f87bcdfe43847967f634000234
PrincePandeycode <pandeyprince4723@gmail.com>
2023-12-18 23:27:11 +0530

    empty dictioniory not taking vars

--- Emplyee_app/tests.py
+++ Emplyee_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class MyTemplateTests(TestCase):
+    def test_my_template(self):
+        # Assuming you have a user created for testing
+        self.client.login(username='PrincePandey123', password='Princepandey')
+        response = self.client.get('add_emp')
+        self.assertTemplateUsed(response, 'add_emp.html')
