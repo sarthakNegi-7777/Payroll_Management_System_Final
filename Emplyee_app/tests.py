b2f2d2ebccee55d3b6b5cf1843917b049a47b138
Sarthak Negi <sarthaknegi7777@gmail.com>
2023-12-21 20:08:28 +0530

    changes in tests.py

--- Emplyee_app/tests.py
+++ Emplyee_app/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 
-class MyTemplateTests(TestCase):
-    def test_my_template(self):
+class MyTemplateTests(TestCase) :
+    def test_my_template(self) :
         # Assuming you have a user created for testing
-        self.client.login(username='PrincePandey123', password='Princepandey')
+        self.client.login(username='SarthakNegi', password='sarthak_123')
         response = self.client.get('add_emp')
         self.assertTemplateUsed(response, 'add_emp.html')
+        
